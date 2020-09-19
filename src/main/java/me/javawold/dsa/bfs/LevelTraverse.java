909cfcc4ae1dade31477a748416bddf0d0c0e6cd
苏倩文 <suqianwen@meicai.cn>
2020-09-19 14:17:51 +0800

    镜像二叉树与bfs

--- src/main/java/me/javawold/dsa/bfs/LevelTraverse.java
+++ src/main/java/me/javawold/dsa/bfs/LevelTraverse.java
@@ -2,11 +2,7 @@ package me.javawold.dsa.bfs;
 
 import me.javawold.dsa.tree.TreeNode;
 
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Deque;
-import java.util.List;
+import java.util.*;
 
 public class LevelTraverse {
 
@@ -39,5 +35,79 @@ public class LevelTraverse {
         return lists;
     }
 
+    /**
+     * 给定一个二叉树，检查它是否是镜像对称的。
+     *
+     * @param root
+     * @return
+     */
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) return true;
+
+        Deque<TreeNode> queue = new LinkedList<>();//可以添加null元素
+        //将根节点的两个子节点紧挨着加入队列；后续向队列中加入节点，也必须保证同一层对称的两个节点紧挨着一起加入队列
+        queue.addLast(root.left);
+        queue.addLast(root.right);
+
+        while (!queue.isEmpty()) {
+            //同时取出同一层对称的两个节点
+            TreeNode left = queue.removeFirst();
+            TreeNode right = queue.removeFirst();
+
+            if (left == null) {
+                if (right != null) {
+                    return false;
+                } else {
+                    //当前层对称的两个节点都为null(对称)，不再判断其下一层了。
+                }
+            } else {
+                if (right == null || right.val != left.val) {
+                    return false;
+                } else {//当前层对称的两个节点都不为null且镜像，将其下一层对称的两个节点紧挨在一起加入队列*/
+                    queue.addLast(left.left);
+                    queue.addLast(right.right);
+                    //
+                    queue.addLast(left.right);
+                    queue.addLast(right.left);
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public boolean isSymmetricV2(TreeNode root) {
+        if (root == null) return true;
+
+        Deque<TreeNode> queue = new ArrayDeque<>();//不能添加null元素
+        queue.addLast(root);
+        TreeNode nullNode = new TreeNode();
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> list = new ArrayList<>(size);
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.removeFirst();
+                if (node != nullNode) {
+                    list.add(node.val);
+                    queue.addLast(node.left != null ? node.left : nullNode);
+                    queue.addLast(node.right != null ? node.right : nullNode);
+                } else {
+                    list.add(null);
+                    queue.addLast(nullNode);
+                    queue.addLast(nullNode);
+                }
+            }
+
+            for (int i = 0, j = size - 1; i < j; i++, j--) {
+                if (list.get(i) == null) {
+                    if (list.get(j) != null) return false;
+                } else if (list.get(j) == null || !list.get(j).equals(list.get(i))) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
 
 }
