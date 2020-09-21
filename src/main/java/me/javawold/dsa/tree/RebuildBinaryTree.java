9a845d543d92bcc177580e9a7bb740561c90be31
苏倩文 <suqianwen@meicai.cn>
2020-09-21 20:15:52 +0800

    利用树的前序遍历序列+中序遍历序列重建二叉树? 递归

---
+++ src/main/java/me/javawold/dsa/tree/RebuildBinaryTree.java
@@ -0,0 +1,80 @@
+package me.javawold.dsa.tree;
+
+/**
+ * <br>
+ * <br>
+ * Copyright: Copyright (c) 2015<br>
+ * Company: 北京云杉世界信息技术有限公司<br>
+ * 
+ *
+ * @author suqianwen 2020年9月21日
+ */
+public class RebuildBinaryTree {
+	
+	/**
+	 * 输入某二叉树的前序遍历和中序遍历的结果，请重建该二叉树。假设输入的前序遍历和中序遍历的结果中都不含重复的数字。
+
+ 
+
+例如，给出
+
+前序遍历 preorder = [3,9,20,15,7]
+中序遍历 inorder = [9,3,15,20,7]
+返回如下的二叉树：
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/zhong-jian-er-cha-shu-lcof
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+	 *
+	 * @param preorder
+	 * @param inorder
+	 * @return
+	 * @author suqianwen 2020年9月21日
+	 */
+	public TreeNode buildTree(int[] preorder, int[] inorder) {
+		return doBuildTree(preorder, 0, inorder, 0, inorder.length - 1);
+	}
+
+	/**
+	 * 
+	 *
+	 * @param preorder     整棵树的前序遍历序列
+	 * @param preorderHead 左/右子树的头节点在其前序遍历序列中的下标
+	 * @param inorder      整棵树的中序遍历序列
+	 * @param inorderStart 左/右子树的中序遍历序列的开始下标
+	 * @param inorderEnd   左/右子树的中序遍历序列的结束下标
+	 * @return
+	 * @author suqianwen 2020年9月21日
+	 */
+	private TreeNode doBuildTree(int[] preorder, int preorderHead, int[] inorder, int inorderStart, int inorderEnd) {
+		if (/*
+			 * preorderHead == preorder.length// ||
+			 */ inorderStart > inorderEnd) {// 子树为空
+			return null;
+		}
+
+		int rootVal = preorder[preorderHead];
+		TreeNode root = new TreeNode(rootVal);
+
+		/** 找到头节点在中序遍历序列中的位置，将中序遍历序列分为头节点的左右子树的中序遍历序列 */
+		int inorderHead = inorderStart;
+		for (; inorderHead <= inorderEnd; inorderHead++) {
+			if (inorder[inorderHead] == rootVal) {
+				break;
+			}
+		}
+
+		// 递归求解 左子树
+		TreeNode left = doBuildTree(preorder, preorderHead + 1, inorder, inorderStart, inorderHead - 1);
+		root.left = left;
+
+		// 递归求解 右子树
+		// 右子树的头节点在其前序遍历序列中的下标等于原头节点下标 + 左子树的节点数
+		TreeNode right = doBuildTree(preorder, preorderHead + (inorderHead - inorderStart), inorder, inorderHead + 1,
+				inorderEnd);
+		root.right = right;
+
+		return root;
+	}
+
+}
