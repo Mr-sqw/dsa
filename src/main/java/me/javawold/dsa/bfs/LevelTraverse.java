82cf1f7134c42d54cf8cde86c4090009e3087fe8
苏倩文 <suqianwen@meicai.cn>
2020-09-21 22:46:07 +0800

    二叉树的层次遍历

--- src/main/java/me/javawold/dsa/bfs/LevelTraverse.java
+++ src/main/java/me/javawold/dsa/bfs/LevelTraverse.java
@@ -36,4 +36,54 @@ public class LevelTraverse {
         return lists;
     }
 
+    /**
+     * 给你一个二叉树，请你返回其按 层序遍历 得到的节点值。 （即逐层地，从左到右访问所有节点）。
+     *
+     *  
+     *
+     * 示例：
+     * 二叉树：[3,9,20,null,null,15,7],
+     *
+     *     3
+     *    / \
+     *   9  20
+     *     /  \
+     *    15   7
+     * 返回其层次遍历结果：
+     *
+     * [
+     *   [3],
+     *   [9,20],
+     *   [15,7]
+     * ]
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/binary-tree-level-order-traversal
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     * @param root
+     * @return
+     */
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        if (root == null) return Collections.emptyList();
+
+        List<List<Integer>> lists = new ArrayList<>();
+        Deque<TreeNode> queue = new ArrayDeque<>();
+        queue.addLast(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> list = new ArrayList<>(size);
+            lists.add(list);
+            for (int i = 0; i < size; i++) {//当前层的所有节点
+                TreeNode node = queue.removeFirst();
+                list.add(node.val);
+
+                //向队列中加入当前层节点的子节点，构成下一层节点。
+                if (node.left != null) queue.addLast(node.left);
+                if (node.right != null) queue.addLast(node.right);
+            }
+        }
+
+        return lists;
+    }
+
 }
