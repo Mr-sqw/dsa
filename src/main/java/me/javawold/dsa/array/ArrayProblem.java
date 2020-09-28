6fb9526b35ebefa4078013e06a7771bd0258abbf
苏倩文 <suqianwen@meicai.cn>
2020-09-29 00:24:15 +0800

    合并两个升序数组

--- src/main/java/me/javawold/dsa/array/ArrayProblem.java
+++ src/main/java/me/javawold/dsa/array/ArrayProblem.java
@@ -57,4 +57,116 @@ public class ArrayProblem {
 		return j;
 	}
 
+	/**
+	 * 给你两个有序整数数组 nums1 和 nums2，请你将 nums2 合并到 nums1 中，使 nums1 成为一个有序数组。
+	 *
+	 *  
+	 *
+	 * 说明:
+	 *
+	 * 初始化 nums1 和 nums2 的元素数量分别为 m 和 n 。
+	 * 你可以假设 nums1 有足够的空间（空间大小大于或等于 m + n）来保存 nums2 中的元素。
+	 *  
+	 * 示例:
+	 *
+	 * 输入:
+	 * nums1 = [1,2,3,0,0,0], m = 3
+	 * nums2 = [2,5,6],       n = 3
+	 *
+	 * 输出: [1,2,2,3,5,6]
+	 *
+	 * 来源：力扣（LeetCode）
+	 * 链接：https://leetcode-cn.com/problems/merge-sorted-array
+	 * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+	 *
+	 * 从小到大归并
+	 * @param arr1
+	 * @param arr2
+	 * @return
+	 */
+	public void merge(int[] nums1, int m, int[] nums2, int n) {
+		if (n==0) return;
+
+		int[] nums1Copy = new int[m];
+		System.arraycopy(nums1,0,nums1Copy,0,m);
+		//
+		int i = 0;
+		int j = 0;
+		int k = 0;
+		while(i<nums1Copy.length && j< nums2.length){
+			if(nums1Copy[i]<nums2[j]){
+				nums1[k++] = nums1Copy[i++];
+			}else{
+				nums1[k++] = nums2[j++];
+			}
+		}
+		while(i<nums1Copy.length){
+			nums1[k++] = nums1Copy[i++];
+		}
+		while(j< nums2.length){
+			nums1[k++] = nums2[j++];
+		}
+	}
+
+	/**
+	 * 从大到小归并
+	 * @param nums1
+	 * @param m
+	 * @param nums2
+	 * @param n
+	 */
+	public void mergeV2(int[] nums1, int m, int[] nums2, int n) {
+		if (n==0) return;
+
+		//
+		/*int i = nums1.length-1;
+		while(i>=0 && nums1[i]==0){i--;}*/
+		int i = m-1;
+		//
+		int j = n-1/*nums2.length-1*/;
+		int k = m+n-1;
+		while(i>=0 && j>=0){
+			if(nums1[i]>nums2[j]){
+				nums1[k--] = nums1[i--];
+			}else{
+				nums1[k--] = nums2[j--];
+			}
+		}
+//		while(i>=0){
+//			nums1[k--] = nums1[i--];
+//		}
+		while(j>=0){
+			nums1[k--] = nums2[j--];
+		}
+	}
+
+	public int[] merge(int[] arr1, int[] arr2){
+		if(arr1 == null || arr1.length == 0){
+			return arr2;
+		}
+
+		if(arr2 == null || arr2.length == 0){
+			return arr1;
+		}
+
+		int[] returnArr = new int[arr1.length+ arr2.length];
+		int i = 0;
+		int j = 0;
+		int k = 0;
+		while(i<arr1.length && j< arr2.length){
+			if(arr1[i]<arr2[j]){
+				returnArr[k++] = arr1[i++];
+			}else{
+				returnArr[k++] = arr2[j++];
+			}
+		}
+		while(i<arr1.length){
+			returnArr[k++] = arr1[i++];
+		}
+		while(j< arr2.length){
+			returnArr[k++] = arr2[j++];
+		}
+		return returnArr;
+	}
+
 }
