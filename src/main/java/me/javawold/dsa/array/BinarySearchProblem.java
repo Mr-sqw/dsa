3520e30dae5f6dc1584e5c676846722bc706c68a
苏倩文 <suqianwen@meicai.cn>
2020-10-01 21:31:55 +0800

    二分搜索

---
+++ src/main/java/me/javawold/dsa/array/BinarySearchProblem.java
@@ -0,0 +1,121 @@
+package me.javawold.dsa.array;
+
+public class BinarySearchProblem {
+
+    /**
+     * 给定一个按照升序排列的整数数组 nums，和一个目标值 target。找出给定目标值在数组中的开始位置和结束位置。
+     *
+     * 你的算法时间复杂度必须是 O(log n) 级别。
+     *
+     * 如果数组中不存在目标值，返回 [-1, -1]。
+     *
+     * 示例 1:
+     *
+     * 输入: nums = [5,7,7,8,8,10], target = 8
+     * 输出: [3,4]
+     * 示例 2:
+     *
+     * 输入: nums = [5,7,7,8,8,10], target = 6
+     * 输出: [-1,-1]
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/find-first-and-last-position-of-element-in-sorted-array
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] searchRange(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return new int[]{-1, -1};
+        }
+
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                int start = mid - 1;
+                while (start >= 0 && nums[start] == target) start--;
+                //
+                int end = mid + 1;
+                while (end < nums.length && nums[end] == target) end++;
+
+                return new int[]{start + 1, end - 1};
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return new int[]{-1, -1};
+    }
+
+    public int[] searchRangeV2(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return new int[]{-1, -1};
+        }
+
+        int start = searchStartIndex(nums, target);
+        if (start == -1) {
+            return new int[]{-1, -1};
+        }
+        //
+        int end = searchEndIndex(nums, target);
+        //
+        return new int[]{start, end};
+    }
+
+    private int searchStartIndex(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                right = mid - 1;//继续向左边找。如果左边没有，后面left还会向右增长补偿直到等于mid(此时搜索区间不合法)。
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        //此时搜索区间为[right, left]，
+        if (left < nums.length && nums[left] == target) {
+            return left;
+        }
+        return -1;
+    }
+
+    private int searchEndIndex(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                left = mid + 1;//继续向右边找。如果右边没有，后面right还会向左减小补偿直到等于mid(此时搜索区间不合法)。
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        //此时搜索区间为[right, left]，
+        if (right >= 0 && nums[right] == target) {
+            return right;
+        }
+        return -1;
+    }
+
+}
