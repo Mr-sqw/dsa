36049f0f5115847e7d2c721102e98b5f4b34e8d7
苏倩文 <suqianwen@meicai.cn>
2020-10-04 02:48:40 +0800

    两数相加

---
+++ src/main/java/me/javawold/dsa/TwoSumProblem.java
@@ -0,0 +1,72 @@
+package me.javawold.dsa;
+
+import me.javawold.dsa.linkedlist.ListNode;
+
+public class TwoSumProblem {
+
+    /**
+     * 给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
+     *
+     * 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+     *
+     * 您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+     *
+     * 示例：
+     *
+     * 输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+     * 输出：7 -> 0 -> 8
+     * 原因：342 + 465 = 807
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/add-two-numbers
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     *
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+
+        if (l2 == null) {
+            return l1;
+        }
+
+        int bitSum = l1.val + l2.val;
+        ListNode head = new ListNode(bitSum % 10);
+        ListNode node = head;
+        while ((l1 = l1.next) != null && (l2 = l2.next) != null) {
+            bitSum = l1.val + l2.val + bitSum / 10;
+            ListNode tmpNode = new ListNode(bitSum % 10);
+            node.next = tmpNode;
+            node = tmpNode;
+        }
+        if (l1 == null) {//!
+            l2 = l2.next;
+        }
+        while (l1 != null) {
+            bitSum = l1.val + bitSum / 10;
+            ListNode tmpNode = new ListNode(bitSum % 10);
+            node.next = tmpNode;
+            node = tmpNode;
+            l1 = l1.next;
+        }
+        while (l2 != null) {
+            bitSum = l2.val + bitSum / 10;
+            ListNode tmpNode = new ListNode(bitSum % 10);
+            node.next = tmpNode;
+            node = tmpNode;
+            l2 = l2.next;
+        }
+        /**/
+        if (bitSum >= 10) {//有进位
+            node.next = new ListNode(1);
+        }
+        return head;
+    }
+
+}
+
+
