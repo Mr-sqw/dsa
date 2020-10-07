0331658bdafcef347114b69b92e602bed9a7e6d7
苏倩文 <suqianwen@meicai.cn>
2020-10-07 15:28:35 +0800

    160. 相交链表

--- src/main/java/me/javawold/dsa/linkedlist/LinkedListProblem.java
+++ src/main/java/me/javawold/dsa/linkedlist/LinkedListProblem.java
@@ -402,4 +402,74 @@ public class LinkedListProblem {
         }
     }
 
+    /**
+     * 160. 相交链表
+编写一个程序，找到两个单链表相交的起始节点。
+
+如下面的两个链表：
+
+
+
+在节点 c1 开始相交。
+
+ 
+
+示例 1：
+
+
+
+输入：intersectVal = 8, listA = [4,1,8,4,5], listB = [5,0,1,8,4,5], skipA = 2, skipB = 3
+输出：Reference of the node with value = 8
+输入解释：相交节点的值为 8 （注意，如果两个链表相交则不能为 0）。从各自的表头开始算起，链表 A 为 [4,1,8,4,5]，链表 B 为 [5,0,1,8,4,5]。在 A 中，相交节点前有 2 个节点；在 B 中，相交节点前有 3 个节点。
+ 
+
+示例 2：
+
+
+
+输入：intersectVal = 2, listA = [0,9,1,2,4], listB = [3,2,4], skipA = 3, skipB = 1
+输出：Reference of the node with value = 2
+输入解释：相交节点的值为 2 （注意，如果两个链表相交则不能为 0）。从各自的表头开始算起，链表 A 为 [0,9,1,2,4]，链表 B 为 [3,2,4]。在 A 中，相交节点前有 3 个节点；在 B 中，相交节点前有 1 个节点。
+ 
+
+示例 3：
+
+
+
+输入：intersectVal = 0, listA = [2,6,4], listB = [1,5], skipA = 3, skipB = 2
+输出：null
+输入解释：从各自的表头开始算起，链表 A 为 [2,6,4]，链表 B 为 [1,5]。由于这两个链表不相交，所以 intersectVal 必须为 0，而 skipA 和 skipB 可以是任意值。
+解释：这两个链表不相交，因此返回 null。
+ 
+
+注意：
+
+如果两个链表没有交点，返回 null.
+在返回结果后，两个链表仍须保持原有的结构。
+可假定整个链表结构中没有循环。
+程序尽量满足 O(n) 时间复杂度，且仅用 O(1) 内存。
+通过次数155,060提交次数273,672
+     *
+     * @param headA
+     * @param headB
+     * @return
+     * @author suqianwen 2020年10月7日
+     */
+    // 若相交，链表A： a+c, 链表B : b+c. a+c+b+c = b+c+a+c 。则会在公共处c起点相遇。若不相交，a +b = b+a 。因此相遇处是NULL
+    // 走到尽头见不到你，于是走过你来时的路，等到相遇时才发现，你也走过我来时的路。
+    // 我先走我的路，再走你的路，你先走你的路，再走我的路，这样咱俩走的路程就一样了，速度一样，那么肯定在咱俩两条路的交叉口相遇，并一起走向终点。
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+		if (headA == null || headB == null) {
+			return null;
+		}
+
+		ListNode nodeA = headA;
+		ListNode nodeB = headB;
+		while (nodeA != nodeB) {
+			nodeA = (nodeA == null) ? headB : nodeA.next;
+			nodeB = (nodeB == null) ? headA : nodeB.next;
+		}
+		return nodeA;
+    }
+
 }
