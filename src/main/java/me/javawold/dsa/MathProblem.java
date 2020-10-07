1ee0c1a084db3772c5827c1a4035d8bab500e35f
苏倩文 <suqianwen@meicai.cn>
2020-10-07 16:05:06 +0800

    136. 只出现一次的数字

--- src/main/java/me/javawold/dsa/MathProblem.java
+++ src/main/java/me/javawold/dsa/MathProblem.java
@@ -102,4 +102,37 @@ public class MathProblem {
         return nums[mid];
     }
 
+    /**
+     * 136. 只出现一次的数字
+给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
+
+说明：
+
+你的算法应该具有线性时间复杂度。 你可以不使用额外空间来实现吗？
+
+示例 1:
+
+输入: [2,2,1]
+输出: 1
+示例 2:
+
+输入: [4,1,2,1,2]
+输出: 4
+     *
+     * @param nums
+     * @return
+     * @author suqianwen 2020年10月7日
+     */
+	public int singleNumber(int[] nums) {
+		if (nums == null || nums.length == 0) {
+			return 0;
+		}
+
+		int num = nums[0];
+		for (int i = 1; i < nums.length; i++) {
+			num ^= nums[i];// 异或运算
+		}
+		return num;
+	}
+
 }
