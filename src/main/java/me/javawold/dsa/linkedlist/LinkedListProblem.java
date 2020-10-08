3b51ff8df2a0629a04f85b207b6f07605887e75b
苏倩文 <suqianwen@meicai.cn>
2020-10-08 13:48:10 +0800

    141. 环形链表

--- src/main/java/me/javawold/dsa/linkedlist/LinkedListProblem.java
+++ src/main/java/me/javawold/dsa/linkedlist/LinkedListProblem.java
@@ -472,4 +472,71 @@ public class LinkedListProblem {
 		return nodeA;
     }
 
+    /**
+     * 141. 环形链表
+给定一个链表，判断链表中是否有环。
+
+如果链表中有某个节点，可以通过连续跟踪 next 指针再次到达，则链表中存在环。 为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。注意：pos 不作为参数进行传递，仅仅是为了标识链表的实际情况。
+
+如果链表中存在环，则返回 true 。 否则，返回 false 。
+
+ 
+
+进阶：
+
+你能用 O(1)（即，常量）内存解决此问题吗？
+
+ 
+
+示例 1：
+
+
+
+输入：head = [3,2,0,-4], pos = 1
+输出：true
+解释：链表中有一个环，其尾部连接到第二个节点。
+示例 2：
+
+
+
+输入：head = [1,2], pos = 0
+输出：true
+解释：链表中有一个环，其尾部连接到第一个节点。
+示例 3：
+
+
+
+输入：head = [1], pos = -1
+输出：false
+解释：链表中没有环。
+ 
+
+提示：
+
+链表中节点的数目范围是 [0, 104]
+-105 <= Node.val <= 105
+pos 为 -1 或者链表中的一个 有效索引 。
+     *
+     * @param head
+     * @return
+     * @author suqianwen 2020年10月8日
+     */
+	public boolean hasCycle(ListNode head) {
+		if (head == null) {
+			return false;
+		}
+
+		ListNode fast = head.next;
+		ListNode slow = head;
+		while (fast != null && fast.next != null) {
+			/* 如果链表有环，则快慢指针一直在环内移动，并且某个时候，快指针一定会赶上慢指针 */
+			if (fast == slow) {
+				return true;
+			}
+			fast = fast.next.next;
+			slow = slow.next;
+		}
+		return fast == slow;// 如果链表无环，则快指针提前到达尾节点，结束
+	}
+
 }
