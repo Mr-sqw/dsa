8435a1b42ed27184c95e357b16942a128a4c3b5b
苏倩文 <suqianwen@meicai.cn>
2020-10-15 01:02:20 +0800

    116. 填充每个节点的下一个右侧节点指针

--- src/main/java/me/javawold/dsa/tree/TreeProblem.java
+++ src/main/java/me/javawold/dsa/tree/TreeProblem.java
@@ -1,13 +1,9 @@
 package me.javawold.dsa.tree;
 
-import java.util.ArrayList;
-import java.util.Deque;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
 import me.javawold.dsa.Builder;
-import me.javawold.dsa.dp.StockProblem;
+
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * <br>
@@ -486,6 +482,90 @@ root = [10,5,-3,3,2,null,11,3,-2,null,1], sum = 8
         //最后右子树
         doFlatten(root.right, list);
     }
+
+	/**
+	 * 116. 填充每个节点的下一个右侧节点指针
+	 * 给定一个完美二叉树，其所有叶子节点都在同一层，每个父节点都有两个子节点。二叉树定义如下：
+	 *
+	 * struct Node {
+	 *   int val;
+	 *   Node *left;
+	 *   Node *right;
+	 *   Node *next;
+	 * }
+	 * 填充它的每个 next 指针，让这个指针指向其下一个右侧节点。如果找不到下一个右侧节点，则将 next 指针设置为 NULL。
+	 *
+	 * 初始状态下，所有 next 指针都被设置为 NULL。
+	 *
+	 *
+	 *
+	 * 示例：
+	 *
+	 *
+	 *
+	 * 输入：{"$id":"1","left":{"$id":"2","left":{"$id":"3","left":null,"next":null,"right":null,"val":4},"next":null,"right":{"$id":"4","left":null,"next":null,"right":null,"val":5},"val":2},"next":null,"right":{"$id":"5","left":{"$id":"6","left":null,"next":null,"right":null,"val":6},"next":null,"right":{"$id":"7","left":null,"next":null,"right":null,"val":7},"val":3},"val":1}
+	 *
+	 * 输出：{"$id":"1","left":{"$id":"2","left":{"$id":"3","left":null,"next":{"$id":"4","left":null,"next":{"$id":"5","left":null,"next":{"$id":"6","left":null,"next":null,"right":null,"val":7},"right":null,"val":6},"right":null,"val":5},"right":null,"val":4},"next":{"$id":"7","left":{"$ref":"5"},"next":null,"right":{"$ref":"6"},"val":3},"right":{"$ref":"4"},"val":2},"next":null,"right":{"$ref":"7"},"val":1}
+	 *
+	 * 解释：给定二叉树如图 A 所示，你的函数应该填充它的每个 next 指针，以指向其下一个右侧节点，如图 B 所示。
+	 *
+	 *
+	 * 提示：
+	 *
+	 * 你只能使用常量级额外空间。
+	 * 使用递归解题也符合要求，本题中递归程序占用的栈空间不算做额外的空间复杂度。
+	 * @param root
+	 * @return
+	 */
+	public Node connect(Node root) {
+		if (root == null)
+			return root;
+
+		Deque<Node> queue = new LinkedList<>();
+		queue.addLast(root);
+		//
+		while (!queue.isEmpty()) {
+			int size = queue.size();
+			Node curr = queue.removeFirst();
+
+			//
+			if (curr.left != null) queue.addLast(curr.left);
+			if (curr.right != null) queue.addLast(curr.right);
+
+			for (int i = 1; i < size; i++) {
+				Node next = queue.removeFirst();
+
+				curr.next = next;
+
+				curr = next;
+
+				//
+				if (curr.left != null) queue.addLast(curr.left);
+				if (curr.right != null) queue.addLast(curr.right);
+			}
+		}
+		return root;
+	}
+
+	class Node {
+		public int val;
+		public Node left;
+		public Node right;
+		public Node next;
+
+		public Node() {}
+
+		public Node(int _val) {
+			val = _val;
+		}
+
+		public Node(int _val, Node _left, Node _right, Node _next) {
+			val = _val;
+			left = _left;
+			right = _right;
+			next = _next;
+		}
+	}
 
 	public static void main(String[] args) {
 		Integer[] arr = { 2, 3, null, 1 };
