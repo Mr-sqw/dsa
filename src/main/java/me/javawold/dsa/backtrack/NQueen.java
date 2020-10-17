67b66b6159e2835953247b505206f2f2b63ee03c
苏倩文 <suqianwen@meicai.cn>
2020-10-17 11:04:24 +0800

    52. N皇后 II

--- src/main/java/me/javawold/dsa/backtrack/NQueen.java
+++ src/main/java/me/javawold/dsa/backtrack/NQueen.java
@@ -1,13 +1,10 @@
 package me.javawold.dsa.backtrack;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
 import me.javawold.dsa.Utils;
+
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 /**
  * n皇后问题<br>
@@ -142,6 +139,82 @@ public class NQueen {
 		}
 
 		return true;
+	}
+
+	/**
+	 * 52. N皇后 II
+	 * n 皇后问题研究的是如何将 n 个皇后放置在 n×n 的棋盘上，并且使皇后彼此之间不能相互攻击。
+	 *
+	 *
+	 *
+	 * 上图为 8 皇后问题的一种解法。
+	 *
+	 * 给定一个整数 n，返回 n 皇后不同的解决方案的数量。
+	 *
+	 * 示例:
+	 *
+	 * 输入: 4
+	 * 输出: 2
+	 * 解释: 4 皇后问题存在如下两个不同的解法。
+	 * [
+	 *  [".Q..",  // 解法 1
+	 *   "...Q",
+	 *   "Q...",
+	 *   "..Q."],
+	 *
+	 *  ["..Q.",  // 解法 2
+	 *   "Q...",
+	 *   "...Q",
+	 *   ".Q.."]
+	 * ]
+	 *
+	 *
+	 * 提示：
+	 *
+	 * 皇后，是国际象棋中的棋子，意味着国王的妻子。皇后只做一件事，那就是“吃子”。当她遇见可以吃的棋子时，就迅速冲上去吃掉棋子。当然，她横、竖、斜都可走一或 N-1 步，可进可退。（引用自 百度百科 - 皇后 ）
+	 *
+	 * @param n
+	 * @return
+	 */
+	public int totalNQueens(int n) {
+		if (n <= 0) {
+			return 0;
+		}
+
+		AtomicInteger count = new AtomicInteger();
+		// Map<Integer, Integer> rowMap = new HashMap<>();// key: row, value: column
+		Map<Integer, Integer> columnMap = new HashMap<>();// key: column, value: row
+		doSolveNQueens(n, /* rowMap, */columnMap, count);
+
+		return count.get();
+	}
+
+	private void doSolveNQueens(int n, /* Map<Integer, Integer> rowMap, */ Map<Integer, Integer> columnMap,
+								AtomicInteger count) {
+		if (n == columnMap.size()) {
+			count.incrementAndGet();
+
+			return;
+		}
+
+		for (int i = 0; i < n; i++) {// 当前层可选择列表
+			// 矩阵“左上”下标为(0, 0)
+			Integer row = /* rowMap */columnMap.size();// 第几层
+			Integer column = i;// 第几列
+			if (isValid(/* rowMap, */columnMap, n, row, column)// 当前层可选择 且 合法
+					) {
+				// 选择：每层选一个
+				// rowMap.put(row, column);
+				columnMap.put(column, row);
+
+				// 递归调用：从下一层继续选择
+				doSolveNQueens(n, /* rowMap, */columnMap, count);
+
+				// 取消选择
+				// rowMap.remove(row);
+				columnMap.remove(column);
+			}
+		}
 	}
 
 	public static void main(String[] args) {
