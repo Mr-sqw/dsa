edbf14ead36a81403b5d8ff19317e4029fa0818c
苏倩文 <suqianwen@meicai.cn>
2020-10-23 17:36:43 +0800

    7. 整数反转

--- src/main/java/me/javawold/dsa/math/MathProblem.java
+++ src/main/java/me/javawold/dsa/math/MathProblem.java
@@ -380,6 +380,50 @@ public class MathProblem {
 		return result;
 	}
 
+	/**
+	 * 7. 整数反转
+给出一个 32 位的有符号整数，你需要将这个整数中每位上的数字进行反转。
+
+示例 1:
+
+输入: 123
+输出: 321
+ 示例 2:
+
+输入: -123
+输出: -321
+示例 3:
+
+输入: 120
+输出: 21
+注意:
+
+假设我们的环境只能存储得下 32 位的有符号整数，则其数值范围为 [−231,  231 − 1]。请根据这个假设，如果反转后整数溢出那么就返回 0。
+	 *
+	 * @param x
+	 * @return
+	 * @author suqianwen 2020年10月23日
+	 */
+	public int reverse(int x) {
+		List<Integer> digits = new ArrayList<>();// 从低位到高位
+		while (x != 0) {
+			int digit = x % 10;
+			digits.add(digit);
+
+			x = x / 10;
+		}
+
+		long weight = 1;
+		long num = 0;
+		for (int i = digits.size() - 1; i >= 0; i--, weight *= 10) {
+			num += digits.get(i) * weight;
+		}
+		if (num > Integer.MAX_VALUE || num < Integer.MIN_VALUE) {
+			return 0;
+		}
+		return (int) num;
+	}
+
 	public static void main(String[] args) {
 		int[] nums = // {4,3,2,7,8,2,3,1};
 				//{ 3, 1, 3, 4, 2 };
@@ -388,7 +432,8 @@ public class MathProblem {
 		new MathProblem().// findDisappearedNumbers(nums);
 				//findDuplicate(nums);
 				//findUnsortedSubarray(nums);
-		        generate(5);
+		        //generate(5);
+				reverse(1534236469);
 	}
 
 }
