1abac517a0da8e77a1ea3305a2b1c0962fce1858
苏倩文 <suqianwen@meicai.cn>
2020-10-23 16:36:11 +0800

    103. 二叉树的锯齿形层次遍历

--- src/main/java/me/javawold/dsa/bfs/LevelTraverse.java
+++ src/main/java/me/javawold/dsa/bfs/LevelTraverse.java
@@ -88,6 +88,66 @@ public class LevelTraverse {
     }
 
     /**
+     * 103. 二叉树的锯齿形层次遍历
+给定一个二叉树，返回其节点值的锯齿形层次遍历。（即先从左往右，再从右往左进行下一层遍历，以此类推，层与层之间交替进行）。
+
+例如：
+给定二叉树 [3,9,20,null,null,15,7],
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+返回锯齿形层次遍历如下：
+
+[
+  [3],
+  [20,9],
+  [15,7]
+]
+     *
+     * @param root
+     * @return
+     * @author suqianwen 2020年10月23日
+     */
+	public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+		if (root == null)
+			return Collections.emptyList();
+
+		List<List<Integer>> lists = new ArrayList<>();
+		Deque<TreeNode> queue = new ArrayDeque<>();
+		queue.addLast(root);
+		while (!queue.isEmpty()) {
+			int size = queue.size();
+			List<Integer> list = new ArrayList<>(size);
+			lists.add(list);
+			for (int i = 0; i < size; i++) {// 当前层的所有节点
+				TreeNode node = queue.removeFirst();
+
+				list.add(node.val);
+
+				// 向队列中加入当前层节点的子节点，构成下一层节点。
+				if (node.left != null)
+					queue.addLast(node.left);
+				if (node.right != null)
+					queue.addLast(node.right);
+			}
+		}
+
+		for (int i = 1; i < lists.size(); i += 2) {
+			List<Integer> list = lists.get(i);
+			for (int j = 0, k = list.size() - 1; j < k; j++, k--) {
+				int temp = list.get(j);
+				list.set(j, list.get(k));
+				list.set(k, temp);
+			}
+		}
+		//
+		return lists;
+	}
+
+    /**
      * 给定一棵二叉树，设计一个算法，创建含有某一深度上所有节点的链表（比如，若一棵树的深度为 D，则会创建出 D 个链表）。返回一个包含所有深度的链表的数组。
      *
      *  
