b0193ed84b2184a3f278ff63b9f893173127a443
苏倩文 <suqianwen@meicai.cn>
2020-10-23 14:37:05 +0800

    125. 验证回文串

--- src/main/java/me/javawold/dsa/dp/Palindrome.java
+++ src/main/java/me/javawold/dsa/dp/Palindrome.java
@@ -197,4 +197,47 @@ https://leetcode-cn.com/problems/longest-palindromic-substring/
 		return true;
 	}
 
+	/**
+	 * 125. 验证回文串
+给定一个字符串，验证它是否是回文串，只考虑字母和数字字符，可以忽略字母的大小写。
+
+说明：本题中，我们将空字符串定义为有效的回文串。
+
+示例 1:
+
+输入: "A man, a plan, a canal: Panama"
+输出: true
+示例 2:
+
+输入: "race a car"
+输出: false
+	 *
+	 * @param s
+	 * @return
+	 * @author suqianwen 2020年10月23日
+	 */
+	public boolean isPalindrome(String s) {
+		if (s == null || s.length() == 0) {
+			return true;
+		}
+
+		char[] chars = s.toCharArray();
+		for (int i = 0, j = chars.length - 1; i < j; i++, j--) {
+			while (i < j && !Character.isLetterOrDigit(chars[i])) {
+				i++;
+			}
+			while (i < j && !Character.isLetterOrDigit(chars[j])) {
+				j--;
+			}
+			if (i < j && (chars[i] != chars[j] && Character.toUpperCase(chars[i]) != Character.toUpperCase(chars[j]))) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public static void main(String[] args) {
+		new Palindrome().isPalindrome("A man, a plan, a canal: Panama");
+	}
+
 }
