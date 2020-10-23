7f060020b9c4a444693ee67e77aaeea7c0f7268c
苏倩文 <suqianwen@meicai.cn>
2020-10-23 17:03:15 +0800

    9. 回文数

--- src/main/java/me/javawold/dsa/dp/Palindrome.java
+++ src/main/java/me/javawold/dsa/dp/Palindrome.java
@@ -236,6 +236,53 @@ https://leetcode-cn.com/problems/longest-palindromic-substring/
 		return true;
 	}
 
+	/**
+	 * 9. 回文数
+判断一个整数是否是回文数。回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
+
+示例 1:
+
+输入: 121
+输出: true
+示例 2:
+
+输入: -121
+输出: false
+解释: 从左向右读, 为 -121 。 从右向左读, 为 121- 。因此它不是一个回文数。
+示例 3:
+
+输入: 10
+输出: false
+解释: 从右向左读, 为 01 。因此它不是一个回文数。
+进阶:
+
+你能不将整数转为字符串来解决这个问题吗？
+	 *
+	 * @param x
+	 * @return
+	 * @author suqianwen 2020年10月23日
+	 */
+	public boolean isPalindrome(int x) {
+		if (x < 0) {
+			return false;
+		}
+
+		List<Integer> digits = new ArrayList<>();
+		while (x != 0) {
+			int digit = x % 10;
+			digits.add(digit);
+
+			x = x / 10;
+		}
+		//
+		for (int i = 0, j = digits.size() - 1; i < j; i++, j--) {
+			if (!digits.get(i).equals(digits.get(j))) {
+				return false;
+			}
+		}
+		return true;
+	}
+
 	public static void main(String[] args) {
 		new Palindrome().isPalindrome("A man, a plan, a canal: Panama");
 	}
